d4a362d707c9938d47f29ead325bd4c54e93aea7
Ajaykumar Singh <ajay.singh920727@gmail.com>
2022-10-16 14:46:48 -0400

    Adding publish capability to generate Cucumber Report after execution

--- src/test/java/starter/TestRunner.java
+++ src/test/java/starter/TestRunner.java
@@ -6,8 +6,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "html:target/cucumber" }, features = "src/test/resources/features", glue = {
-		"amazonTests" })
+@CucumberOptions(
+		plugin = { "pretty", "html:target/cucumber" }, 
+		features = "src/test/resources/features", 
+		glue = {"amazonTests" },
+		publish = true)
 public class TestRunner {
 
 }
